7f1f9e35413aabae4f27bc86f052b13384b4a847
jsanti <valen500@gmail.com>
2020-08-08 00:01:41 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FiltrosComponent } from './consulta/filtros/filtros.component';
+import { ResultadosComponent } from './consulta/resultados/resultados.component';
+import { ConsultaComponent } from './consulta/consulta/consulta.component';
+import { PlayerComponent } from './consulta/player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FiltrosComponent,
+    ResultadosComponent,
+    ConsultaComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
